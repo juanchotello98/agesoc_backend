d3392dc7990e797a1db3de7a134fd4fe1dc408a5
juanchotello98 <tello.juan@correounivalle.edu.co>
2020-09-15 14:23:01 -0500

    borrado modelo cargo

--- apps/cargo/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from apps.cargo.viewsets import CargoViewSet
-
-router = routers.SimpleRouter()
-router.register('cargos', CargoViewSet)
-
-urlpatterns = router.urls
